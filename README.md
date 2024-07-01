e0287fa3f5b951badf6cd4e4e71c2ffff6607cec
Olga.Gubkina <63593441+OlGubkina@users.noreply.github.com>
2024-07-01 18:05:20 +0200

    cyprress start

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # CypressExperiments
+
+npm init                        создаст package.json
+npm install cypress             утановит cypress
+npx cypress open
+
+Чтобы cypress не запускал тесты при каждом изменении
+в файле cypress.config.js
+добавить watchForFileChanges: false,
+
---
+++ cypress/e2e/hillel.cy.js
@@ -0,0 +1,41 @@
+//const { beforeEach } = require("mocha")
+
+/* const enterUserInfo = function() {
+        cy.get('#signupName').type('Nameee')
+        cy.get('#signupLastName').type('LastNameee')
+        cy.get('#signupEmail').type('Nameee.last@gmail.com')
+        cy.get('#signupPassword').type('12345678Na')
+        cy.get('#signupRepeatPassword').type('12345678Na')
+} */
+
+describe('Sgn up into hillel',()=> {
+
+    beforeEach (()=>{
+        cy.visit('https://guest:welcome2qauto@qauto.forstudy.space/')
+    })
+
+    it ('verify user can sign in', ()=> {
+        
+        cy.get('.hero-descriptor_btn').click()
+        cy.get('#signupName').type('Nameee')
+        cy.get('#signupLastName').type('LastNameee')
+        cy.get('#signupEmail').type('Nameee.last@gmail.com')
+        cy.get('#signupPassword').type('12345678Na')
+        cy.get('#signupRepeatPassword').type('12345678Na')      
+        cy.get('button').contains('Register').click()
+        // cy.get('element').should('have text', env === 'Prod' ? 'Prod text' : 'Dev text')
+    })
+
+    it.only('test', {retries: 1}, ()=> { // остальные будут skipped // основная попытка + 1 ретрай
+        cy.get('.hero-descriptor_btn', { timeout: 10000 }).click() 
+        // будет ждать 10 сек прогрузки элемента (4 debug)
+        // задаётся в конфиге глобальной переменной
+    })
+
+    it.skip('test', ()=> { // не будут запущены
+        cy.get('.hero-descriptor_btn').click()
+    })
+
+    after (()=>{cy.log('After block')})
+})
+    
